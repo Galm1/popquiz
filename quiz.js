719547bb82d6bdf5163312d40f0018c0f0becbfd
EliGarcia <garciaelisha93@yahoo.com>
2017-08-24 14:28:10 -0500

    done

--- quiz.js
+++ quiz.js
@@ -1,46 +1,37 @@
 console.log('whatever')
 
 
-
-
 function begin() {
-	console.log("begin");
+  console.log("begin");
 }
 begin();
 
 
-
-
 function echo(x) {
-console.log(x);
+  console.log(x);
 }
 echo('hello');
 
 
-
-
-function math(x,y,z) {
-let add = x + y;
-let answer = add * z;
-console.log(answer)
+function math(x, y, z) {
+  let add = x + y;
+  let answer = add * z;
+  console.log(answer)
 }
-math(6,4,9);
+math(6, 4, 9);
 
 
-//
-// let array = ['here ','is ','my ','string.'];
 function toString(array) {
-let string = array.join('');
-console.log(string);
+  let string = array.join('');
+  console.log(string);
 }
-toString(['here ','is ','my ','string.']);
-
-
+toString(['here ', 'is ', 'my ', 'string.']);
 
 
 function callme(callback) {
   console.log('first from callme')
-  function callback(){
+
+  function callback() {
     console.log('second from callme');
   }
   callback();
@@ -48,20 +39,16 @@ function callme(callback) {
 callme();
 
 
-
-
-
 function callme2(callback) {
   console.log('first from callme2')
-  function callback(hello){
+
+  function callback(hello) {
     console.log('second from callme2');
     console.log(hello);
   }
   callback('hello in param of callme2');
 }
 callme2();
-
-
 
 
 function reverse(word) {
@@ -72,14 +59,13 @@ function reverse(word) {
 reverse('Reverse Me');
 
 
-
 function sleep() {
-setTimeout(function () {
-  console.log('before sleep "loop"')
-}, 1000);
-setTimeout(function () {
-  console.log('after sleep "loop"')
-}, 2000)
+  setTimeout(function() {
+    console.log('before sleep "loop"')
+  }, 1000);
+  setTimeout(function() {
+    console.log('after sleep "loop"')
+  }, 2000)
 
 }
 sleep();
