233166bfa87f942c4c438d094b2df49c1b47f658
EliGarcia <garciaelisha93@yahoo.com>
2017-08-24 14:09:50 -0500

    all answered. not all are correct

--- quiz.js
+++ quiz.js
@@ -1,1 +1,84 @@
 console.log('whatever')
+
+
+
+
+function begin() {
+	console.log("begin");
+}
+begin();
+
+
+
+
+function echo(x) {
+console.log(x);
+}
+echo('hello');
+
+
+
+
+function math(x,y,z) {
+let add = x + y;
+let answer = add * z;
+console.log(answer)
+}
+math(6,4,9);
+
+
+//
+// let array = ['here ','is ','my ','string.'];
+function toString(array) {
+let string = array.join();
+console.log(string);
+}
+toString(['here ','is ','my ','string.']);
+
+
+
+
+function callme(callback) {
+  console.log('first from callme')
+  function callback(){
+    console.log('second from callme');
+  }
+  callback();
+}
+callme();
+
+
+
+
+
+function callme2(callback) {
+  console.log('first from callme2')
+  function callback(hello){
+    console.log('second from callme2');
+    console.log(hello);
+  }
+  callback('hello in param of callme2');
+}
+callme2();
+
+
+
+
+function reverse(word) {
+let blah = word.split();
+console.log(blah)
+}
+reverse('Reverse Me');
+
+
+
+function sleep() {
+setTimeout(function () {
+  console.log('before sleep "loop"')
+}, 1000);
+setTimeout(function () {
+  console.log('after sleep "loop"')
+}, 2000)
+
+}
+sleep();
